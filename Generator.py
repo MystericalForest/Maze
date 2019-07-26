fb998b8b1cadfd66dd73d03904890b7ea5bc294f
Kasper Alveen - KPA <KPA@ATP.DK>
2019-07-26 07:34:40 +0200

    Some cleanup

--- Generator.py
+++ Generator.py
@@ -4,16 +4,17 @@
 import random
 from PIL import Image
 import Solver
+import settings
 
-imgx = 500; imgy = 500
-image = Image.new("RGB", (imgx, imgy))
+#imgx = 500; imgy = 500
+image = Image.new("RGB", (settings.imgx, settings.imgy))
 pixels = image.load()
-mx = 50; my = 50 # width and height of the maze
-maze = [[0 for x in range(mx)] for y in range(my)]
+#mx = 50; my = 50 # width and height of the maze
+maze = [[0 for x in range(settings.mx)] for y in range(settings.my)]
 dx = [0, 1, 0, -1]; dy = [-1, 0, 1, 0] # 4 directions to move in the maze
 color = [(0, 0, 0), (255, 255, 255), (0, 255, 0), (0, 0, 255)] # RGB colors of the maze
 # start the maze from a random cell
-cx = random.randint(0, mx - 1); cy = random.randint(0, my - 1)
+cx = random.randint(0, settings.mx - 1); cy = random.randint(0, settings.my - 1)
 maze[cy][cx] = 1; stack = [(cx, cy, 0)] # stack element: (x, y, direction)
 
 while len(stack) > 0:
@@ -29,12 +30,12 @@ while len(stack) > 0:
     nlst = [] # list of available neighbors
     for i in dirRange:
         nx = cx + dx[i]; ny = cy + dy[i]
-        if nx >= 0 and nx < mx and ny >= 0 and ny < my:
+        if nx >= 0 and nx < settings.mx and ny >= 0 and ny < settings.my:
             if maze[ny][nx] == 0:
                 ctr = 0 # of occupied neighbors must be 1
                 for j in range(4):
                     ex = nx + dx[j]; ey = ny + dy[j]
-                    if ex >= 0 and ex < mx and ey >= 0 and ey < my:
+                    if ex >= 0 and ex < settings.mx and ey >= 0 and ey < settings.my:
                         if maze[ey][ex] == 1: ctr += 1
                 if ctr == 1: nlst.append(i)
 
@@ -46,37 +47,37 @@ while len(stack) > 0:
     else: stack.pop()
 
 # paint the maze
-for ky in range(imgy):
-    for kx in range(imgx):
-        px=int((mx+2) * kx / imgx)-1
-        py=int((my+2) * ky / imgy)-1
-        if (py<0) or (px<0) or (py>=my) or (px>=mx):
+for ky in range(settings.imgy):
+    for kx in range(settings.imgx):
+        px=int((settings.mx+2) * kx / settings.imgx)-1
+        py=int((settings.my+2) * ky / settings.imgy)-1
+        if (py<0) or (px<0) or (py>=settings.my) or (px>=settings.mx):
             if ((py<0) and (px<1)) or ((px<0) and (py<1)):
                 pixels[kx, ky] = color[1]
-            elif ((py>=my) and (px>=mx-1)) or ((px>=mx) and (py>=my-1)):
+            elif ((py>=settings.my) and (px>=settings.mx-1)) or ((px>=settings.mx) and (py>=settings.my-1)):
                 pixels[kx, ky] = color[1]
             else:
                 pixels[kx, ky] = color[0]
         else:
             pixels[kx, ky] = color[maze[py][px]]
-image.save("output\\Maze_" + str(mx) + "x" + str(my) + ".png", "PNG")
+image.save("output\\Maze_" + str(settings.mx) + "x" + str(settings.my) + ".png", "PNG")
 
 solver=Solver.AStar()
-solver.init_grid(mx, my, maze)
+solver.init_grid(settings.mx, settings.my, maze.copy())
 solver.process()
 
 # paint the mazetrip
-for ky in range(imgy):
-    for kx in range(imgx):
-        px=int((mx+2) * kx / imgx)-1
-        py=int((my+2) * ky / imgy)-1
-        if (py<0) or (px<0) or (py>=my) or (px>=mx):
+for ky in range(settings.imgy):
+    for kx in range(settings.imgx):
+        px=int((settings.mx+2) * kx / settings.imgx)-1
+        py=int((settings.my+2) * ky / settings.imgy)-1
+        if (py<0) or (px<0) or (py>=settings.my) or (px>=settings.mx):
             if ((py<0) and (px<1)) or ((px<0) and (py<1)):
                 pixels[kx, ky] = color[1]
-            elif ((py>=my) and (px>=mx-1)) or ((px>=mx) and (py>=my-1)):
+            elif ((py>=settings.my) and (px>=settings.mx-1)) or ((px>=settings.mx) and (py>=settings.my-1)):
                 pixels[kx, ky] = color[1]
             else:
                 pixels[kx, ky] = color[0]
         else:
             pixels[kx, ky] = color[solver.grid[py][px]]
-image.save("output\\Maze_" + str(mx) + "x" + str(my) + "_solved.png", "PNG")
+image.save("output\\Maze_" + str(settings.mx) + "x" + str(settings.my) + "_solved.png", "PNG")
---
+++ settings.py
@@ -0,0 +1,4 @@
+imgx = 500  # Image width
+imgy = 500  # Image height
+mx = 30     # Maze width
+my = 30     # Maze height
